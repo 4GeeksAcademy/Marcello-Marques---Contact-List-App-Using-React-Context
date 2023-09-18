c0eed5235a8bfbc20fc3ec11c6bcb37adebd8347
Marcello-Marques <137443029+Marcello-Marques@users.noreply.github.com>
2023-09-18 20:34:11 +0000

    Fetch Agenda

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,22 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+    const baseUrl = "https://playground.4geeks.com/apis/fake/contact/agenda/";
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+    return {
+        store: {
+            contact: [],
+            agendaData: [] // Initialize agendaData in the store
+        },
+        actions: {
+            getContacts: () => {
+				fetch(baseUrl)
+                    .then((received) => received.json())
+                    .then((data) => setStore({ contact: data }))
+                    .catch ((error)=>{
+                     console.error(`Error fetching data for: ${error.message}`);
+                    }) 
+                }
+        },
+    };
 };
 
 export default getState;
---
+++ src/js/views/Contacts.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import { ContactCard } from "../component/ContactCard.js";
+import { Modal } from "../component/Modal";
+
+export const Contacts = () => {
+	const { store, actions } = useContext(Context);
+	console.log("store", store);
+	const [state, setState] = useState({
+		showModal: false
+	});
+
+	return (
+		<div className="container">
+			<div>
+				<p className="text-right my-3">
+					<Link className="btn btn-success" to="/add">
+						Add new contact
+					</Link>
+				</p>
+				<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
+					<ul className="list-group pull-down" id="contact-list">
+						{store.contact ? store.contact.map((item,index)=>{
+							return(
+								<div>
+									<ContactCard onDelete={() => setState({ showModal: true })} />
+								</div>
+							)
+						}):"No contact"}
+					</ul>
+				</div>
+			</div>
+			<Modal show={state.showModal} onClose={() => setState({ showModal: false })} />
+		</div>
+	);
+};
